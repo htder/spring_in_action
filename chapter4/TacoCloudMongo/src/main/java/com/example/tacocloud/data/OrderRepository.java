d6d73216f4a8eb1d534070f4abaa3efdeaf9c2f6
Henry Derbyshire <henry@htderbyshire.com>
2022-05-20 21:06:29 +0100

    updated the orderRepository to work with the changed taco order class

--- chapter4/TacoCloudMongo/src/main/java/com/example/tacocloud/data/OrderRepository.java
+++ chapter4/TacoCloudMongo/src/main/java/com/example/tacocloud/data/OrderRepository.java
@@ -3,5 +3,5 @@ package com.example.tacocloud.data;
 import com.example.tacocloud.tacos.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 }
