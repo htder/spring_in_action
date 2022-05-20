536001ef0558f6fec2b796c6237e8b7973ea1de6
Henry Derbyshire <henry@htderbyshire.com>
2022-05-20 13:08:00 +0100

    created ingredient user defined type

---
+++ chapter4/TacoCloudCassandra/src/main/java/com/example/tacocloud/tacos/IngredientUDT.java
@@ -0,0 +1,17 @@
+package com.example.tacocloud.tacos;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.cassandra.core.mapping.UserDefinedType;
+
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+@UserDefinedType("ingredient")
+public class IngredientUDT {
+
+    private final String name;
+
+    private final Ingredient.Type type;
+}
